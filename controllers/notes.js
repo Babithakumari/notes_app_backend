ad4f4d578a26d617bcdc934f7d8ecde233790d83
Babithakumari <babithakumari120@gmail.com>
2023-06-13 22:50:10 +0530

    part4 completed

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,8 +2,15 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/notes')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
-
+const getTokenFrom = request => {  
+	const authorization = request.get('authorization')  
+	if (authorization && authorization.startsWith('Bearer ')) {    
+		return authorization.replace('Bearer ', '') 
+	}  
+	return null
+}
 
 
 // GET ALL NOTES
@@ -55,7 +62,14 @@ notesRouter.post('/', async(request,response) => {
 	const body = request.body
     
 	// get the user
-	const user = await User.findById(body.userId)
+	// eslint-disable-next-line no-undef
+	console.log(getTokenFrom(request))
+	// eslint-disable-next-line no-undef
+	const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)  
+	if (!decodedToken.id) {    
+		return response.status(401).json({ error: 'token invalid' })  
+	}
+	const user = await User.findById(decodedToken.id)
 
 	// make a new note
 	const note = new Note({
